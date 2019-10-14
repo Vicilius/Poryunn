8e8fc715fb8f43221e75f3e8b3a0df9a56381d0c
GabrielMart-hub <21701296@aluno.cotemig.com.br>
2019-10-14 15:06:55 -0300

    mudancasLegais
    usu

---
+++ A Poe Without Arms/Assets/Scripts/Fantasma.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fantasma : MonoBehaviour
+{
+
+    private bool colide = false;
+    private float move = -2;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move, GetComponent<Rigidbody2D>().velocity.y);
+        if (colide)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        move *= -1;
+        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+        colide = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bool tagMundo = collision.gameObject.CompareTag("mundo");
+        bool tagBlocoTrancado = collision.gameObject.CompareTag("bloco-trancado");
+        bool tagFantasma = collision.gameObject.CompareTag("fantasma");
+        if (tagMundo || tagBlocoTrancado || tagFantasma)
+        {
+            colide = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("mundo"))
+        {
+            colide = false;
+        }
+    }
+
+
+}
